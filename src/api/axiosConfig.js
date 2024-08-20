c98df967fc6c52f459913282b2d62e4d1798a7c4
sourabh-swargam <swargam.sourabh@gmail.com>
2024-08-20 16:09:33 -0500

    updated link

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://movies-api-app-b6inf4mg7a-vp.a.run.app",
+  baseURL: "https://movie-api-b6inf4mg7a-vp.a.run.app",
   headers: { "Content-Type": "application/json" },
 });
